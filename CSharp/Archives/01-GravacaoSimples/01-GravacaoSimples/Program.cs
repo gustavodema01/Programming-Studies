19ffcfe04d7c6922806dc3e8888fcd751577f41a
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-08-04 11:39:37 -0300

    Treino com arquivos

---
+++ CSharp/Archives/01-GravacaoSimples/01-GravacaoSimples/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_GravacaoSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> name = new List<string>();
+            string path = @"c:\temp\arquivos.txt";
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Digite o {i} nome: ");
+                string nome = Console.ReadLine();
+                name.Add(nome); //adiciono os nomes na lista
+            }
+
+            try
+            {
+                using (StreamWriter fs = File.AppendText(path))//streamwriter para abrir o arquivo
+                    foreach (string names in name)
+                    {
+                        fs.WriteLine(names); //escrevo cada nome como uma linha do arquivo
+                    }
+                using (StreamReader sr = File.OpenText(path)) //streamreader para a leitura 
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e}");
+            }
+        }
+    }
+}
+
