680b97397862a37a1de52ff81d6d7ae0017b24c6
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-08-23 11:49:16 -0300

    feat: exercicios de POO (Faculdade) - classe Jogo

---
+++ CSharp/Faculdade/ex1 POO/ex1 POO/Jogo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1_POO
+{
+    internal class Jogo
+    {
+
+        public int Codigo { get; private set; } // o private permite que a váriavel só seja mudada dentro da classe
+        public string Nome { get; private set; }
+        public string Categoria { get; private set; }
+        public DateTime DataLancamento { get; private set; }
+
+        public Jogo(int codigo, string nome, string categoria, DateTime dataLancamento) //construtor para obrigar as variáveis a serem iniciadas
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Categoria = categoria;
+            DataLancamento = dataLancamento;
+        }
+
+        public override string ToString()
+        {
+            Console.Clear();
+            return $"\nCódigo: {Codigo}\n Nome: {Nome}\n Categoria: {Categoria}\n Data de lançamento: {DataLancamento:dd/MM/yyyy}";
+        }
+
+    }
+}
---
+++ CSharp/Faculdade/ex1 POO/ex1 POO/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ex1_POO
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.Write("Código: "); //a responsabilidade de recolher as informações do usuário é do programa, não da classe
+                int codigo = int.Parse(Console.ReadLine());
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Categoria: ");
+                string categoria = Console.ReadLine();
+
+                Console.Write("Data de Lançamento: ");
+                DateTime dataLancamento = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                Jogo jogo = new Jogo(codigo, nome, categoria, dataLancamento); //instaciação da classe com as variáveis declaradas
+                Console.WriteLine(jogo);
+            }
+            catch (Exception ex) //tratamento de erro
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
