017210598323c6f8a3e3a7c70f8eb3b81bd7d513
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-08-27 22:34:04 -0300

    treino com subselect

--- Database/Aulas/2026-08-20 Subselect e join.sql
+++ Database/Aulas/2026-08-20 Subselect e join.sql
@@ -4,7 +4,6 @@ select f.func_id, f.func_nome, f.func_salario,
 		c.cargo_nome
 from Funcionarios f
 inner join cargos c on c.cargo_id = f.cargo_id
- 
 --usando agora o subselect
 select f.func_nome,
 		(select a.ano from apontamentos a
@@ -14,75 +13,68 @@ select f.func_nome,
 		where a.func_id = f.func_id and
 			a.ano = 2025 and a.mes = 7) horas
 			from Funcionarios f
- 
 --Mostrar a quantidade de funcionarios que cada cargo possui
- 
 --com join(preste atenção ao uso do LEFT
 select c.cargo_nome, count(f.func_id) qtd
 from cargos c
 left join funcionarios f on c.cargo_id = f.cargo_id
 group by c.cargo_nome
 order by qtd
- 
 --com subselect
 select c.cargo_nome,
 		(select count(*) from Funcionarios f
 		where f.cargo_id = c.cargo_id) qtd
 from cargos c
 order by qtd
- 
 --------------------------------------------------------
 --mostra todos os funcionarios que possuir em seu cargo a palavra 'Jr'
 select * 
 from Funcionarios f
 where f.cargo_id in (select c.cargo_id from cargos c
 					 where c.cargo_nome like '%jr')
- 
 --com join
 select * 
 from Funcionarios f
 left join cargos c on f.cargo_id = c.cargo_id
 where c.cargo_nome like '%jr'
- 
 ------------------------------------
 --usando o subselect dentro de clausula FROM
 select*
 from (select * from Funcionarios f
 	  where f.func_salario > 6000) temp
 inner join cargos c on c.cargo_id = temp.cargo_id
- 
 --sem subselect
 select f.func_nome, c.cargo_nome
 from Funcionarios f
 inner join cargos c on c.cargo_id = f.cargo_id
 where f.func_salario > 6000
-  
 --Para cada exercício resolva-o com JOIN e dps com SubSelect
 --1)Liste todos os dados dos filmes e o nome do genero
 --com join
+use Locadora
 select m.CodMidia, m.DescMidia, m.ValorUnit, m.codGenero, m.quantidade, g.descricao as nomeGenero
 from Midias m
 inner join Genero g on m.CodGenero = g.CodGenero
-
+ 
 --com SubSelect
 select *, (select descricao from Genero g
 			where m.Codgenero = g.Codgenero ) as nomeGenero 
 			from Midias m
-
+ 
 --2)Liste o nome do cliente e a quantidade de locações feitas por ele(join e group)
 --com join
 select c.cliente, count(l.CodLocacao) as qtdLocacao
 from Cliente c
-left join Locacao l on c.CodCli = l.CodCli
-group by c.cliente
-
+left join Locacao l on c.CodCli = l.Co
+ 
 --com subselect
 select c.cliente,(select count(l.CodLocacao)
 				 from Locacao l
-				 where c.CodCli = l.CodCli) as qtdLocacao
+				 where c.CodCli = l.CodCdCli
+group by c.clienteli) as qtdLocacao
 from Cliente c
 order by c.cliente
-
+ 
 --3)Resolva o exercício anterior mostrando somente os clientes que possuem mais que 2 locações(join,group e having)
 --com join
 select c.cliente, count(l.CodLocacao) as qtdLocacao
@@ -90,8 +82,35 @@ from Cliente c
 left join Locacao l on c.CodCli = l.CodCli
 group by c.cliente
 having count(l.CodLocacao)>2
+ 
+--com subselect
+select c.cliente, (select count(l.CodLocacao) 
+					from Locacao l 
+					where c.CodCli = l.CodCli ) as qtdLocacao
+from Cliente c
+where (select count(l.CodLocacao) 
+					from Locacao l 
+					where c.CodCli = l.CodCli ) >2
+ 
+--4)Liste para cada locação a data da locação, o nome do filme e o nome do cliente(join)
+--com join
+select l.CodLocacao, l.DataLoc, c.Cliente, DescMidia
+from Locacao l
+INNER JOIN Cliente c on l.CodCli = c.CodCli
+INNER JOIN ItensLocacao i on l.CodLocacao = i.CodLocacao
+INNER JOIN Midias m on i.CodMidia = m.CodMidia
+ 
+--com subselect
+select l.CodLocacao,()
+ 
+--5)Faça a soma de todos os valores unitários de todos os filmes locados para cada locação. Mostrando o numero da locação e sua somatório só JOINS
+select itl.CodLocacao, sum(m.ValorUnit) soma
+from ItensLocacao itl
+inner join Midias m on itl.CodMidia = m.CodMidia
+group by itl.CodLocacao
 
 --com subselect
-
---4)Liste para cada locação a data da locação, o nome do filme e o nome do cliente(join)
---5)Faça a soma de todos os valores unitários de todos os filmes locados para cada locação. Mostrando o numero da locação e sua somatório só JOINS
+select distinct itl.CodLocacao, (select sum(m.valorUnit) from Midias m
+						where m.CodMidia in (select CodMidia from ItensLocacao x
+						where x.CodLocacao = itl.codLocacao))
+from ItensLocacao itl
