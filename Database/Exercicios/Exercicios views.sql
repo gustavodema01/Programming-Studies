6504bdd8428c68b5c030b77600e91410c5f475ca
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-09-03 22:26:03 -0300

    Exercicio para fazer

---
+++ Database/Exercicios/Exercicios views.sql
@@ -0,0 +1,24 @@
+/*
+Usando ao banco de dados de locadora faça:
+1) Crie uma view chamada vw_nuncaLocados, que exiba
+o nome, codigo dos filmes e genero que nunca foram alugados
+
+2) Crie uma view chamada vw_ClienteSemLocacao, que exiba
+o nome e codigo dos clientes que nunca fizeram uma locação
+
+3) Crie uma view chamada vw_Top5, que exiba
+o nome e codigo dos 5 filmes mais alugados
+
+4) Crie uma view chamada vw_LocacaoTOP, que exiba o 
+nome dos clientes de maneira distinta que fizeram locações
+de algum dos filmes Top5 (use a view do ex_3)
+
+5) Crie uma view chamada vw_genero, que exiba o 
+codigo e descrição do genero de todos os filmes que nunca
+foram locados (use view do ex_1)
+
+6) Cria uma view chamada vw_ClienteTOP, que exiba
+os 5 nomes de clientes com codigo, que fizeram o maior
+numero de locações
+*/
+
