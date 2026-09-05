71f68bb20109ef12be1df179e21fca552ceeee62
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-09-05 20:50:51 -0300

    feat: continuação - cadastro e listagem de até 100 funcionários

---
+++ CSharp/Faculdade/AtividadePOO - Cadastro com lista - Funcionario/AtividadePOO - Cadastro com lista - Funcionario/Entities/Funcionário.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Atividade_POO
+{
+    internal class Funcionário
+    {
+        private string _nome; //private porque eles só podem serem modificados nessa classe funcionario
+        private double _salarioBase; //private porque eles só podem serem modificados nessa classe funcionario
+        public DateTime DataNascimento { get; set; }
+        public double Salariofinal { get; set; }
+        public string Nome
+        {
+            get  //get retorna o valor
+            {   return _nome;  }
+            set //set atualiza o valor
+            {    if (string.IsNullOrWhiteSpace(value)) //validação
+                {
+                    throw new ArgumentException("O nome é obrigatório."); //excessão criada por mim
+                }
+                _nome = value;//valor final
+            }
+        }
+
+        public double SalarioBase
+        {
+            get { return _salarioBase; }
+            set
+            {
+                if (value <= 0) //validação
+                {  throw new ArgumentException("O salário deve ser maior que zero."); } //excessão que eu criei
+                _salarioBase = value;
+            }
+        }
+
+        public Funcionário(string nome) //construtor para a variável ser inciada obrigatóriamente.
+        {
+            Nome = nome;
+        }
+        public double SalarioLiquido(double DescontoMes) // método
+        {
+            return SalarioBase - DescontoMes;
+        }
+
+        public double SalarioLiquido(double DescontoMes, double Bonus)//sobrecarga do método SalarioLiquido
+        {
+            Salariofinal = SalarioLiquido(DescontoMes) + Bonus;
+            return Salariofinal;
+        }
+
+        public override string ToString()
+        {
+            return "\nNome: " + Nome +
+                   "\nSalário base: " + SalarioBase+
+                   "\nSalário final: " + Salariofinal;
+        }
+    }
+}
