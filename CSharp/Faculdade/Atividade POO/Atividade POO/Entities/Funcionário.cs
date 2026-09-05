d926518eddf17815aeda592bc62df04e18654f41
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-09-05 14:57:54 -0300

    feat: adiciona atividade POO - Funcionário

---
+++ CSharp/Faculdade/Atividade POO/Atividade POO/Entities/Funcionário.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Atividade_POO
+{
+    internal class Funcionário
+    {
+        private string _nome; //private porque eles só podem serem modificados nessa classe funcionario
+        private double _salarioBase; //private porque eles só podem serem modificados nessa classe funcionario
+        public DateTime DataNascimento { get; set; }
+        public string Nome
+        {
+            get  //get retorna o valor
+            {
+                return _nome;
+            }
+            set //set atualiza o valor
+            {
+                if (string.IsNullOrWhiteSpace(value)) //validação
+                {
+                    throw new ArgumentException("O nome é obrigatório."); //excessão criada por mim
+                }
+                _nome = value;//valor final
+            }
+        }
+
+        public double SalarioBase
+        {
+            get
+            {
+                return _salarioBase;
+            }
+            set
+            {
+                if (value <= 0) //validação
+                {
+                    throw new ArgumentException("O salário deve ser maior que zero."); //excessão que eu criei
+                }
+                _salarioBase = value;
+            }
+        }
+
+        public Funcionário(string nome, double salarioBase, DateTime dataNascimento) //construtor para as variáveis serem inciadas obrigatóriamente.
+        {
+            Nome = nome; 
+            SalarioBase = salarioBase; 
+            DataNascimento = dataNascimento; 
+        }
+
+        public double SalarioLiquido(double DescontoMes) // método
+        {
+            return SalarioBase - DescontoMes;
+        }
+
+        public double SalarioLiquido(double DescontoMes, double Bonus)//sobrecarga do método SalarioLiquido
+        {
+            return SalarioLiquido(DescontoMes) + Bonus;
+        }
+    }
+}
+
---
+++ CSharp/Faculdade/Atividade POO/Atividade POO/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Atividade_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Nome: "); //atribui a responsabilidade de coletar os dados fora da classe
+                string nome = Console.ReadLine(); 
+
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine());
+
+                Console.Write("Data de nascimento: ");
+                DateTime nascimento = DateTime.Parse(Console.ReadLine());
+
+                Funcionário f = new Funcionário(nome, salario, nascimento); //instanciação com os parâmetros atribuidos
+
+                Console.Write("Desconto do mês: ");
+                double desconto = double.Parse(Console.ReadLine());
+                double salarioDescontado = f.SalarioLiquido(desconto); //chamei o método e coloquei o parâmetro
+
+                Console.Write("Bônus do mês: ");
+                double bonus = double.Parse(Console.ReadLine());
+                double salariofinal = f.SalarioLiquido(bonus, desconto); 
+
+                Console.WriteLine("\nNome: " + nome +
+                    "\nData de nascimento: " + nascimento +
+                    "\nSalario pós desconto" +
+                    "\nSalário pós bônus: " + salariofinal);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("\nMensagem de erro: " + ex.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Mensagem de erro: " + e.Message);
+            }
+        }
+    }
+}
