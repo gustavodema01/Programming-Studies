5ad0f466708551cb04c1be74e7dc099133d05276
Gustavo Dos Santos <gustavo.demagsp@gmail.com>
2026-09-05 23:03:34 -0300

    feat: exercicios de POO da faculdade

---
+++ CSharp/Faculdade/POO - Faculdade/07-08 - Pessoa e Funcionario (Heranca)/AtividadePOO - Pessoa - Funcionario/AtividadePOO - Pessoa - Funcionario/Entities/Funcionário.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AtividadePOO4___Pessoa.Entities
+{
+    internal class Funcionario : Pessoa
+    {
+        private double _salario; //private para não permitir que o nome seja alterado fora da classe
+        public double Salario
+        {
+            get { return _salario; }
+            set
+            {
+                if (value < 0)//validação para o salário ser maior que 0
+                { throw new ArgumentException("Salário inválido!"); }
+                _salario = value;
+            }
+        }
+        public Funcionario(string nome, int idade, double salario) : base(nome, idade)
+        {
+            Salario = salario;
+        }
+
+        public override string DevolveNome()
+        {
+            return base.DevolveNome().ToUpper();
+        }
+    }
+}
